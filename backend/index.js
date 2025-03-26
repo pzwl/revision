7e8e67f6ed42dcb003261b4c4c8f571a3e048727
pzwl <sharmaprajjwal333@gmmail.com>
2025-03-26 23:14:44 +0530

    backend done

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 const { CreateTodo, UpdateTodo } = require("./types");
+const todoModel = require("./db");
 app.use(express.json());
 
 app.listen(3000, () => {
@@ -9,7 +10,7 @@ app.listen(3000, () => {
 });
 
 
-app.post("/", function (req, res) {
+app.post("/todo", async function (req, res) {
    const createpayload = req.body;
    const validatepayload = CreateTodo.safeParseparse(createpayload);
 
@@ -18,17 +19,33 @@ app.post("/", function (req, res) {
    }
 
    // put it in mongo db
+  await todoModel.create({
+    title: createpayload.title,
+    description: createpayload.description,
+    completed: false,
+  });
+  return res.status(200).json({message: "Todo created successfully"});
 });
 
-app.get("/", function (req, res) {
-  
+app.get("/todos", async function (req, res) {
+  const todos = await todoModel.find();
+  return res.status(200).json({todos});
 });
 
-app.put("/", function (req, res) {
+app.put("/complete/:id", async function (req, res) {
     const updatepayload = req.body;
     const validatepayload = UpdateTodo.safeParseparse(updatepayload);
  
     if(!validatepayload.success){
      return res.status(411).json({error: validatepayload.error});
     }
+
+    const todo = await todoModel.findByIdAndUpdate(req.params.id, {completed: true});
+    return res.status(200).json({message: "Todo completed successfully"});
+
+});
+
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
 });
